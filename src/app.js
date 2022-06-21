a80734644046f72324b5f42f482bc8f528f38af0
Mariia-cioccolatini <107420122+Mariia-cioccolatini@users.noreply.github.com>
2022-06-21 20:19:47 +0200

    added Pressure

--- src/app.js
+++ src/app.js
@@ -26,7 +26,9 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let pressureElement = document.querySelector("#pressure");
   let windElement = document.querySelector("#wind");
+
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icone");
 
@@ -36,7 +38,9 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  pressureElement.innerHTML = response.data.main.pressure;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
