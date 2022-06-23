127ea57dfbae2858d8a0c47ae6944794686af09f
Mariia-cioccolatini <107420122+Mariia-cioccolatini@users.noreply.github.com>
2022-06-23 13:42:40 +0200

    fixed icon

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
 
   let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icone");
+  let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
 
